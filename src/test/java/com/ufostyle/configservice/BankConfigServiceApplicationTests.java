f74082e82ffe0d7045bf7d9c6888e72a93dbf389
Nick <nicksaurinocruz@gmail.com>
2024-02-08 16:51:47 -0500

    The config server was implemented for the project.

---
+++ src/test/java/com/ufostyle/configservice/BankConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufostyle.configservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
